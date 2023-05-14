7cfcf2779216ef4f1cdc315941a354b4c7c3e83e
Marcio Henrique <dev.marcioh@gmail.com>
2023-05-14 18:43:55 -0300

    other changes

--- abacada-front/src/components/gameCard/index.tsx
+++ abacada-front/src/components/gameCard/index.tsx
@@ -2,19 +2,19 @@ import React from "react";
 import "./style.css";
 
 //O componente Game pega os dados dos jogos e os renderiza na tela
-function GameCard(props: {name: string, description: string, image: string}) {
+function GameCard(props: {nome: string, descricao: string, image: string, url: string}) {
 
   return (
     <div className="backgroundGameCard">
       <div className="imageContainerGameCard">
-        <img src={require(`../../assets/${props.image}`)} className="imageGameCard"/>
+        <img src={require("../../assets/jogo2.jpg")} className="imageGameCard"/>
       </div>
       <div className="dataContainerGameCard">
         <div className="titleGameCard">
-          {props.name}
+          {props.nome}
         </div>
         <div className="descriptionGameCard">
-          {props.description}
+          {props.descricao}
         </div>
       </div>
     </div>
--- abacada-front/src/pages/students/index.tsx
+++ abacada-front/src/pages/students/index.tsx
@@ -2,42 +2,47 @@ import React, { useEffect, useState } from "react";
 import Card from "../../components/card";
 import { studentsType } from "../../@types/types";
 import "./style.css";
-import { useParams } from "react-router-dom";
+import { useNavigate, unstable_HistoryRouter } from "react-router-dom";
+import { MdArrowBack } from "react-icons/md";
 import userServices from "../../services/userServices";
 
 //pagina dos alunos
 function Students() {
-  const { professor } = useParams();
-  const registroProfessor = parseInt(professor || "-1");
+  const navigate = useNavigate();
+
+  const professor = sessionStorage.getItem("professor");
   const [students, setStudents] = useState<studentsType[]>([]);
   const [error, setError] = useState("");
+  const [isDeleteMode, setIsDeleteMode] = useState(false);
+  const [isUpdateMode, setIsUpdateMode] = useState(false);
 
   useEffect(() => {
-    if(registroProfessor == -1) {
-      setError("Professor não encontrado");
-    }
     userServices.getStudents(professor)
       .then(res => setStudents(res))
       .catch(error => setError(error));
-  }, []);
+  }, [isDeleteMode, isUpdateMode]);
 
   return (
     <div className="backgroundStudents">
       <div className="background-leftStudents">
+        
         <img src={require("../../assets/Logo.png")} className="logoImage"/>
       </div>
       <div className="background-rightStudents">
         <div className="containerStudents">
-        <div className="labelTitleStudents">
+          <div className="labelTitleStudents">
             <label>Alunos</label>
           </div>
           <div className="contentStudents">
             {students.map((student) =>
-              <Card key={student.registro} registro={student.registro} nome={student.nome} situation="Aluno"/>
+              <Card key={student.registro} id={student.registro} nome={student.nome} situation="Aluno" delete={isDeleteMode} update={isUpdateMode}/>
             )}
-
-            <Card registro={registroProfessor} nome="Novo Aluno" situation="AddAluno"/>
           </div>
+          <div className="containerButtons">
+            <input type="submit" value="Excluir" className="button" onClick={() => setIsDeleteMode(!isDeleteMode)}></input>
+            <input type="submit" value="Alterar" className="button" onClick={() => setIsUpdateMode(!isUpdateMode)}></input>
+            <input type="submit" value="Cadastrar" className="button" onClick={() => navigate("/addAluno")}></input>
+          </div>  
         </div>
       </div>
 
