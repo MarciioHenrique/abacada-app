8d2882b8f0e73e13ade416a0cf34d40feec400c9
Marcio Henrique <dev.marcioh@gmail.com>
2023-05-14 18:42:08 -0300

    sessionStorage working

--- abacada-front/src/components/card/index.tsx
+++ abacada-front/src/components/card/index.tsx
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./style.css";
-import { RxUpdate } from "react-icons/rx";
+import { BiPencil } from "react-icons/bi";
 import { AiOutlineDelete } from "react-icons/ai";
 import userServices from "../../services/userServices";
 
 //Componente Card vai pegar as informações do aluno/professor e mostrar na tela
-function Card(props: {registro: number, nome: string, situation: string, delete: boolean, update: boolean}) {
+function Card(props: {id: string, nome: string, situation: string, delete: boolean, update: boolean}) {
   const navigate = useNavigate();
+  const [isConfirmationPopUpVisible, setIsConfirmationPorpUpVisible] = useState(false);
 
- 
 
   function handleClick() {
     if(props.delete || props.update) 
@@ -17,13 +17,12 @@ function Card(props: {registro: number, nome: string, situation: string, delete:
     
     switch (props.situation) {
       case "Professor":
-        navigate("/alunos/"+props.registro);
+        sessionStorage.setItem("professor", props.id);
+        navigate("/alunos");
         break;
       case "Aluno":
+        sessionStorage.setItem("aluno", props.id);
         navigate("/home");
-        break;
-      case "AddAluno":
-        navigate("/addAluno/"+props.registro);
         break;
       default:
         break;
@@ -31,9 +30,19 @@ function Card(props: {registro: number, nome: string, situation: string, delete:
   }
 
   function handleClickDelete() {
-    userServices.deleteTeacher(props.registro)
+    if (props.situation == "Professor") {
+      userServices.deleteTeacher(props.id)
       .then(res => location.reload())
       .catch(error => alert(error));
+    }
+    else if (props.situation == "Aluno") {
+      userServices.deleteStudent(props.id)
+      .then(res => location.reload())
+      .catch(error => alert(error));
+    }
+    else {
+      alert("ERRO");
+    }
   }
 
   function handleClickUpdate() {
@@ -44,7 +53,7 @@ function Card(props: {registro: number, nome: string, situation: string, delete:
     <div>
       <div className="contentCard" onClick={handleClick}>
         <div className="optionsCard">
-          {props.update ? <RxUpdate className="updateMode" onClick={handleClickUpdate} style={{visibility: "visible"}}/> : <RxUpdate className="updateMode" onClick={handleClickUpdate} style={{visibility: "hidden"}}/>}
+          {props.update ? <BiPencil className="updateMode" onClick={handleClickUpdate} style={{visibility: "visible"}}/> : <BiPencil className="updateMode" onClick={handleClickUpdate} style={{visibility: "hidden"}}/>}
           {props.delete ? <AiOutlineDelete className="deleteMode" onClick={handleClickDelete} style={{visibility: "visible"}}/> : <AiOutlineDelete className="deleteMode" onClick={handleClickDelete} style={{visibility: "hidden"}}/>}
         </div>
         <div className="imageContainerCard">
@@ -54,6 +63,7 @@ function Card(props: {registro: number, nome: string, situation: string, delete:
           {props.nome}
         </div>
       </div>
+    
     </div>
   );
 }
