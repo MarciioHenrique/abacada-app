18f4159aa81a137cb1aa344a3d40022f9cca7176
Marcio Henrique <dev.marcioh@gmail.com>
2023-09-27 21:46:53 -0300

    Novo card do jogo

--- abacada-front/src/components/game-card.tsx
+++ abacada-front/src/components/game-card.tsx
@@ -1,17 +1,29 @@
 import React from "react";
 import "../styles/components/game-card.css";
 
-//O componente Game pega os dados dos jogos e os renderiza na tela
-function GameCard(props: {id: string, nome: string, image: string, url: string}) {
-  
+interface GameCardProps {
+  id: string;
+  nome: string;
+  descricao: string;
+  image: string;
+  url: string;
+}
 
+//O componente Game pega os dados dos jogos e os renderiza na tela
+function GameCard(props: GameCardProps) {
   const handleClick = () => {
     sessionStorage.setItem("jogo", props.id);
   };
 
   return (
-    <div className="image-container-game-card">
+    <div className="background-game-card">
+      <div className="image-conteiner-game-card">
         <img src={require(`../assets/${props.image}`)} className="image-game-card" onClick={handleClick}/>
+      </div>
+      <div className="data-conteiner-game-card">
+        <div className="name-game-card">{props.nome}</div>
+        <div className="description-game-card">{props.descricao}</div>
+      </div>  
     </div>
   );
 }
